f909989398fc47055febe938d45ac125cba37020
huoxudong125 <423huo@163.com>
2022-02-11 16:00:59 +0800

    Add 通过hashSet来统计最长的不重复字符串长度

--- String-process/src/main/java/org/hqf/tutorials/java/algorithm/SubStr.java
+++ String-process/src/main/java/org/hqf/tutorials/java/algorithm/SubStr.java
@@ -1,11 +1,47 @@
 package org.hqf.tutorials.java.algorithm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SubStr {
 
     public static void main(String[] args) {
-        int result = getLongestSubstring("abcabcebb");
+        String str = "abcabcebb";
+        int result = getLongestSubstring(str);
         System.out.println("result = " + result);
+
+        int subStrLength = getLengthOfLongestSubstring(str);
+        System.out.println("subStrLength = " + subStrLength);
+
     }
+
+    /**
+     * 获取最长不重复子串的长度
+     *
+     * @param s
+     * @return
+     */
+    public static int getLengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<Character>();
+        int count = s.length();
+        int right = -1, subLength = 0;
+        for (int left = 0; left < count; ++left) {
+
+            if (left != 0) {
+                set.remove(s.charAt(left - 1));
+            }
+
+            while (right + 1 < count
+                    && !set.contains(s.charAt(right + 1))) {
+                set.add(s.charAt(right + 1));
+                ++right;
+            }
+
+            subLength = Math.max(subLength, right - left + 1);
+        }
+        return subLength;
+    }
+
 
     public static int getLongestSubstring(String s) {
         int length = s.length();
